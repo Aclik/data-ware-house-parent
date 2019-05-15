d0019ff9bd80724c4608dce72b76a1f37066a68e
Aclik <1253668861@qq.com>
2019-05-15 09:21:33 +0800

    Initial commit by wenXiang.Chen

---
+++ data-ware-house-mock/src/main/java/com/yxbuild/bean/AppErrorLog.java
@@ -0,0 +1,13 @@
+package com.yxbuild.bean;
+
+import lombok.Data;
+
+/**
+ * 错误日志
+ */
+@Data
+public class AppErrorLog {
+    private String errorBrief;    //错误摘要
+
+    private String errorDetail;   //错误详情
+}
